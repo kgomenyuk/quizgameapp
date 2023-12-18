f75c124fe7a8f469d26992dadcc05ed3badfc14e
artemten <artemten81@gmail.com>
2023-12-19 00:05:23 +0300

    Added a readme to appPointsSender

--- launch/main_quizgame.js
+++ launch/main_quizgame.js
@@ -7,36 +7,33 @@ UIMessage.debug = true;
 
 // all apps available
 let mapping = [
-    {
-        alias: "game2",
-        newInstance: () => new AppGameQuiz()
-    },
-    {
-        alias: "course",
-        newInstance: () => new AppCourses()
-    },
-    {
-        alias: "sf",
-        newInstance: () => {
-            var app = new AppSurveyFields();
-            //app.startCommand = "me";
-            //app.filterGroupId = "2";
-            //app.availableSurveyCodes = ["studInfo"];
-            //app.refCode = "cloud";
-            return app;
-        }
-    },
-    {
-        alias: "grcloud",
-        newInstance: () => {
-            var app = new AppPointsSender();
-            app.refCode = "cloud";
-            return app;
-        }
-    }];
+{
+    alias: "game2",
+    newInstance: () => new AppGameQuiz()
+},
+{
+alias: "sf",
+newInstance: () => {
+var app = new AppSurveyFields();
+//app.startCommand = "me";
+//app.filterGroupId = "2";
+//app.availableSurveyCodes = ["studInfo"];
+//app.refCode = "cloud";
+return app;
+}
+},
+{
+alias: "grcloud",
+newInstance: () => {
+var app = new AppPointsSender();
+app.refCode = "cloud";
+return app;
+}
+}];
     
-    module.exports = {
-        name:"Game2 config",
-        description: "Quiz game",
-        mapping: mapping
-    };
+
+module.exports = {
+    name:"Game2 config",
+    description: "Quiz game",
+    mapping: mapping
+};
